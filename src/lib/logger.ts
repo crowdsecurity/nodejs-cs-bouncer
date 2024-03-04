c48d824533dff06a172a9d1db730cb59872ba778
Roussange Alexandre <roussange.alexandre@gmail.com>
2024-03-04 10:07:28 +0100

    feat(dx): :loud_sound: integrate pino and pino-pretty (#1)

---
+++ src/lib/logger.ts
@@ -0,0 +1,15 @@
+import pino from 'pino';
+
+const LOG_LEVEL = process.env.LOG_LEVEL ?? 'debug';
+
+const logger = pino({
+    level: LOG_LEVEL,
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+        },
+    },
+});
+
+export default logger;
