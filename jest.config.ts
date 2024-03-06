dc2eb9600184fdc36976fdafda7095db9473aba8
emmanuelvacher <emmanuel.vacher@crowdsec.net>
2024-03-06 14:19:12 +0100

    ci: set up code coverage with Codecov

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+    collectCoverage: true,
+    coverageReporters: ['text', 'cobertura'],
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
