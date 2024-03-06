dbef91ee8ab9f8278e581180ec9a174586b00070
Roussange Alexandre <roussange.alexandre@gmail.com>
2024-03-06 16:31:49 +0100

    chore(dx): :rotating_light: update ESlint rules to strengthen the code (#3)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+    ],
     overrides: [
         {
             env: {
@@ -20,5 +25,53 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['@typescript-eslint'],
+    settings: {
+        'import/resolver': {
+            typescript: {
+                alwaysTryTypes: true,
+                project: './tsconfig.json',
+            },
+        },
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+    },
+    rules: {
+        'import/no-unresolved': 'error',
+        'import/order': [
+            'error',
+            {
+                groups: [
+                    'external',
+                    ['builtin', 'index', 'sibling', 'parent', 'internal'],
+                    'object',
+                    'type',
+                ],
+                'newlines-between': 'always',
+                alphabetize: { order: 'asc', caseInsensitive: true },
+            },
+        ],
+        'no-relative-import-paths/no-relative-import-paths': [
+            'error',
+            { allowSameFolder: false },
+        ],
+        'prefer-arrow-functions/prefer-arrow-functions': [
+            'error',
+            {
+                allowNamedFunctions: false,
+                classPropertiesAllowed: false,
+                disallowPrototype: false,
+                returnStyle: 'unchanged',
+                singleReturnOnly: false,
+            },
+        ],
+        'prefer-template': 'error',
+        'no-nested-ternary': 'error',
+    },
+    plugins: [
+        '@typescript-eslint',
+        'prefer-arrow-functions',
+        'eslint-plugin-import',
+        'no-relative-import-paths',
+    ],
 };
