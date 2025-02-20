08a4749989aebab3a5e4d7577ae62bff59b95606
Julien Loizelet <julienloizelet@okaeli.com>
2025-02-20 17:47:22 +0900

    ci(playwright): Create on report by test

--- examples/express-server/playwright.config.ts
+++ examples/express-server/playwright.config.ts
@@ -8,6 +8,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, 'crowdsec/.env') });
+const currentDateTime = new Date().toISOString().replace(/[:.]/g, '_').slice(0, -1);
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -19,11 +20,20 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 1 : 0,
+    retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: [
+        [process.env.CI ? 'github' : 'list'],
+        [
+            'html',
+            {
+                open: 'never',
+                outputFolder: process.env.CI ? `playwright-report/${currentDateTime}` : 'playwright-report',
+            },
+        ],
+    ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
